eac60ab039f19e89526c908396a5e01b421509e2
Alex C <acoundou@gmail.com>
2023-08-15 12:44:02 +0300

    Initial commit

---
+++ lib/src/test/java/appletransactionclient/AppleTransactionClientTest.java
@@ -0,0 +1,14 @@
+package appletransactionclient;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppleTransactionClientTest {
+
+    @Test
+    @DisplayName("Temp Test")
+    void tempTest() {
+
+    }
+
+}
